ff23e24ecc306f48c12825bffc808761e5ce38d9
Bittu Gurrala <bittugurrala241@gmail.com>
2026-02-18 17:10:46 +0530

    Commit

--- src/components/RecentNotes.jsx
+++ src/components/RecentNotes.jsx
@@ -1,6 +1,6 @@
 export default function RecentNotes(){
     return(
-        <div className="h-screen w-screen md:w-1/2 p-5 border-l-2 border-amber-50">
+        <div className="h-screen w-screen md:w-1/2 p-5 border-git remote set-url origin https://github.com/username/newrepo.gitl-2 border-amber-50">
             <h3 className="text-2xl font-bold">Recent Notes</h3>
         </div>
     )
